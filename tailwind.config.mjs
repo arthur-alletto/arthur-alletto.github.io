6daa47cbe9bd640fef061ddc8ac5a8420ad8ae45
arthur-alletto <arthur.alletto@gmail.com>
2024-11-26 17:28:04 +0100

    26/11

---
+++ tailwind.config.mjs
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+
+			},
+
+			fontFamily: {
+				'dmsans': ["DM Sans", "sans-serif"],
+				'dynapuff': ["DynaPuff", "sans-serif"],
+			},
+
+			fontWeight: {
+				thin: '100',
+				extralight: '200',
+				light: '300',
+				regular: '400',
+				medium: '500',
+				semibold: '600',
+				bold: '700',
+				extrabold: '800',
+				black: '900',
+			}
+		},
+	},
+	plugins: [],
+}
